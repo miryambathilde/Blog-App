92fd2cfa2c377c339eb6685963ee4a9fb89098e1
MiryamBathilde <mbathilde@gmail.com>
2022-08-24 23:18:41 +0200

    views form, post and index done and boostrap added & author and post associated

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,8 @@ class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  # here I associate author to post
+  has_many :posts 
 end
